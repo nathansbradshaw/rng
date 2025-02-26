a09fa552fbfb034feacd0dfc72f97c5c48cac121
Nathan Bradshaw <nathansbradshaw@gmail.com>
2025-02-25 20:13:37 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "all",
+  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
+  theme: {
+    extend: {
+      dropShadow: {
+        blue: "0 0 2em #01A7D5"
+      },
+      keyframes: {
+        dioxus: {
+          "0%, 100%": { transform: "translateY(0px)"},
+          "33%": { transform: "translateY(-10px)"},
+          "66%": { transform: "translateY(10px)"},
+        },
+        tailwind: {
+          "0%, 100%": { transform: "translateY(0px)"},
+          "33%": { transform: "translateY(10px)"},
+          "66%": { transform: "translateY(-10px)"},
+        },
+      },
+      animation: {
+        dioxus: "dioxus 1s ease-in-out",
+        tailwind: "tailwind 1s ease-in-out",
+      }
+    },
+  },
+  variants: {},
+  plugins: []
+}
