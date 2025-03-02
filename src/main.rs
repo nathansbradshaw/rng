c3396f8242d2214e9a09d6586cdee700cc72f3c5
Nathan Bradshaw <nathansbradshaw@gmail.com>
2025-03-02 16:49:01 -0600

    i don't know anymore

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,28 @@ const MAIN_CSS: Asset = asset!("/assets/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::new()
+    // Set the server config only if we are building the server target
+    .with_cfg(server_only! {
+        ServeConfig::builder()
+            // Enable incremental rendering
+            .incremental(
+                IncrementalRendererConfig::new()
+                    // Store static files in the public directory where other static assets like wasm are stored
+                    .static_dir(
+                        std::env::current_exe()
+                            .unwrap()
+                            .parent()
+                            .unwrap()
+                            .join("public")
+                    )
+                    // Don't clear the public folder on every build. The public folder has other files including the wasm
+                    // binary and static assets required for the app to run
+                    .clear_cache(false)
+            )
+            .enable_out_of_order_streaming()
+    })
+    .launch(App);
 }
 
 #[component]
